19b51d01b142e2d4ff1648fd85dc2f23bc0b84c3
terasi <hitokunn750@gmail.com>
2024-09-15 20:21:13 +0900

    Constructor ensyu5

--- Test.java
+++ Test.java
@@ -13,6 +13,18 @@ public class Test {
         System.out.println(jiro.name);
         System.out.println(jiro.age);
 
+        Person saburo=new Person("saburo");
+        System.out.println(saburo.name);
+        System.out.println(saburo.age);
+
+        Person nanasi=new Person(25);
+        System.out.println(nanasi.name);
+        System.out.println(nanasi.age);
+
+        Person hanako=new Person(17,"hanako");
+        System.out.println(hanako.name);
+        System.out.println(hanako.age);
+
 	}
 
 }
